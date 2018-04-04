152fc7c92fc5b9ced397f9d9ee8db8c93ab728ce
Sergei Sevriugin <sevriugin@gmail.com>
2018-04-04 22:26:21 +0300

    TokenPool init version

--- contracts/TokenPool.sol
+++ contracts/TokenPool.sol
@@ -37,6 +37,11 @@ import './TokenContainer.sol';
 ///   2                      |______SubPool
 ///   3                                |_______Token
 contract TokenPool is TokenContainer {
+    /// TokenPool insertPool event
+    /// @param id inserted token ID
+    /// @param poolId pool ID
+    /// @param level pool level
+    event InsertPool(uint256 id, uint256 poolId, uint8 level);
     /// @dev Pool defines pool structure
     /// @param level Pool level: 0,1,2,3
     /// @param maxNumber Maximum number of pools on this lavel
@@ -53,15 +58,81 @@ contract TokenPool is TokenContainer {
     }
     /// @dev Pool structure 
     Pool[] pools;           // Pool structure
+    /// inset new member in the pool
+    /// @param _id NFT token ID to inserr
+    /// @param _level Pool level to insert
+    /// @return TRUE if insert is done 
+    function _insertPool(uint256 _id, uint8 _level) internal returns (bool) {
+        uint256 parentId = pools[_level].last;      // pool NFT token ID
+        uint256 size = _getPoolSize(parentId);      // current pool size
+        uint256 max = pools[_level].maxMember - 1;  // max pool size - 1
+        // check if there is a place to insert 
+        if (size < max) {
+            // simple insert
+            addToken(_id, parentId); // add to pool 
+            InsertPool(_id, parentId, _level); // event 
+            return true;
+        }
+        else {
+            // no capacity in the current pool, so, need to add new pool
+            // check if it's possible
+            if(pools[_level].number == pools[_level].maxNumber) {
+                // no capacity to add new pool at this lavel
+                return false;
+            }
+            else {
+                // make a copy from the last pool
+                uint newPool = _createNFT(uint256(0), nfts[parentId].metadata, nfts[parentId].kind, owner);
+                if (newPool != uint(0)) { 
+                    // insert pool in the pool structure
+                    if (_insertPool(newPool, _level-1)) {
+                        // insert token to the new pool
+                        addToken(_id, newPool); // add to pool
+                        InsertPool(_id, newPool, _level); // event 
+                        // record new pool data in the structure 
+                        pools[_level].last = newPool; // new pool is last one
+                        pools[_level].number++;
+                        // new pool and member are inserted 
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+    /// insert token in pool structure
+    /// @param _id NFT token ID to insert
+    /// @return TRUE if insert is done 
+    function insertPool(uint256 _id) public returns(bool) {
+        require(_id != uint256(0));
+        return _insertPool(_id, 2);
+    }
+    /// TokenPool Constructor
     function TokenPool(string _name, string _symbol) TokenContainer(_name, _symbol) public { 
         maxLevel = 4;
+
+        // Creating templates
+        uint superPoolId = super._createNFT(uint256(0), "SuperPool", uint256(1), owner);
+        uint poolId = super._createNFT(uint256(0), "Pool", uint256(1), owner);
+        uint subPoolId = super._createNFT(uint256(0), "SubPool", uint256(2), owner);
+
+        // Build initil structure SubPool --> Pool --> SuperPool
+        addToken(poolId, superPoolId);
+        addToken(subPoolId, poolId);
+
+        // Build configuration 
         // SuperPool configuration
         Pool memory superPool = Pool({
             level: uint8(0),
             maxNumber: uint256(1),
             maxMember: uint256(10),
-            number: uint256(0),
-            last: uint256(0),
+            number: uint256(1),
+            last: uint256(superPoolId),
             share: uint256(10)
         });
         pools.push(superPool);
@@ -70,8 +141,8 @@ contract TokenPool is TokenContainer {
             level: uint8(1),
             maxNumber: uint256(10),
             maxMember: uint256(100),
-            number: uint256(0),
-            last: uint256(0),
+            number: uint256(1),
+            last: uint256(poolId),
             share: uint256(20)
         });
         pools.push(pool);
@@ -80,8 +151,8 @@ contract TokenPool is TokenContainer {
             level: uint8(2),
             maxNumber: uint256(1000),
             maxMember: uint256(100),
-            number: uint256(0),
-            last: uint256(0),
+            number: uint256(1),
+            last: uint256(subPoolId),
             share: uint256(50)
         });
         pools.push(subPool);
