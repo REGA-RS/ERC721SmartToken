b882b821e66907f6f8c0d5d8bc6d0d26694741bd
Sergei Sevriugin <sevriugin@gmail.com>
2018-04-18 17:53:02 +0300

    reload join function

--- contracts/LuggageCrowdsurance.sol
+++ contracts/LuggageCrowdsurance.sol
@@ -28,9 +28,48 @@
 pragma solidity ^0.4.17;
 
 import './TokenCrowdsurance.sol';
+import './interfaces/IERC20Token.sol';
 
 /// Luggage crowdsurance protection product based on TokenCrowdsurance NFT token 
 contract LuggageCrowdsurance is TokenCrowdsurance {
-    function LuggageCrowdsurance() TokenCrowdsurance("Luggage Crowdsurance NFT", "LCS") public {
+    /// REGA Risk Sharing Token smart contract address
+    IERC20Token public  RST;                // RST smart contract address
+    uint256     public  joinAmountRST;      // Join amount in RST
+    bool        public  RSTOnly;            // Join only for RST tokens
+    uint8       public  maxHold;            // Maximum number of toikens for one address
+    /// join function
+    /// @return cowdsuranceId NFT token ID for created crowdsurance
+    function join() public payable returns(uint256 cowdsuranceId) {
+        uint256 amount = msg.value;
+        address member = msg.sender;
+        uint256 score = addressToScore[member];
+        require(score != uint256(0));
+        require(balanceOf(member) < maxHold);
+
+        if (RSTOnly) {
+            // now need to check that the member has approved the transfer joinAmountRST to join
+            require(RST != address(0));                     // check that we have valid contract address
+            require(joinAmountRST != uint256(0));           // join RST amount must be > 0
+            uint256 rstAmt = RST.allowance(member, owner);  // check if the member has gave permision to spend some amount
+            require(rstAmt >= joinAmountRST);               // ... and check if allowance is more then joinAmount
+            amount = addressToAmount[member];               // get join amount and check...
+            require(rstAmt >= amount && amount >= joinAmountRST);
+            // transfer join RST amount to the owner account
+            require(RST.transferFrom(member, owner, amount));
+        }
+        else {
+            require(amount != uint256(0) && amount >= parameters.joinAmount);
+            require(amount == addressToAmount[member]);
+        }
+        // call internal _join after all checkups 
+        cowdsuranceId = _join(member, score, amount);
+    }
+    function LuggageCrowdsurance(address _rst, uint256 _amount, bool _only, uint8 _max) 
+                TokenCrowdsurance("Luggage Crowdsurance NFT", "LCS") public {
+        // setting up contract parameters 
+        RST = IERC20Token(_rst);
+        joinAmountRST = _amount; 
+        RSTOnly = _only; 
+        maxHold = _max;
     }
 }
